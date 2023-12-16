73e9c8f695416bdbf80801011fd495ce8a379828
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-16 15:48:52 +0300

    refactor: tor tests

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -5,8 +5,8 @@ public class BasicVariablesInitialization {
         int first = 1;
         int second = 10;
         int third = 100;
-        System.out.println("first = " + first);
-        System.out.println("second = " + second);
-        System.out.println("third = " + third);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -5,20 +5,20 @@ public class ReassigningValues {
         int first = 1;
         int second = 10;
         int third = 100;
-        System.out.println("first = " + first);
-        System.out.println("second = " + second);
-        System.out.println("third = " + third);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
         int linkToFirst = first;
         int linkToSecond = second;
         int linkToThird = third;
         first = 15;
         second = 6;
         third = 4;
-        System.out.println("first = " + first);
-        System.out.println("second = " + second);
-        System.out.println("third = " + third);
-        System.out.println("linkToFirst = " + linkToFirst);
-        System.out.println("linkToSecond = " + linkToSecond);
-        System.out.println("linkToThird = " + linkToThird);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
